33e575452f3dae11e995db1c4e4a0ec1a7927c10
shifuxiang <591317029@qq.com>
2017-06-14 14:50:14 +0800

    修改位序错误插入的表为oristarmr.app_monitor_rank
    修改位序错误插入的表为oristarmr.app_monitor_rank，同时修改位序错误的开始结束时间为龙标位置减去到该广告要出现在正确位置的相对秒数为开始时间，开始时间加上该广告的长度为结束时间

--- order_error.sql
+++ order_error.sql
@@ -264,6 +264,8 @@ BEGIN
 	DECLARE correct_adprevious VARCHAR(64);
 	DECLARE correct_adback VARCHAR(64);
 	DECLARE correct_count INT DEFAULT 0;
+	DECLARE correct_imglen 	INT DEFAULT 0;
+	DECLARE correct_imgcount INT DEFAULT 0;
 
 	DECLARE monitor_order INT DEFAULT 0;
 	DECLARE monitor_hallno INT DEFAULT 0;
@@ -273,7 +275,7 @@ BEGIN
 	DECLARE monitor_city VARCHAR(32);
 	DECLARE monitor_cinemaname VARCHAR(36);
 	DECLARE monitor_count INT DEFAULT 0;
-	DECLARE monitor_id		VARCHAR(36);
+	DECLARE monitor_ids		VARCHAR(36);
 	DECLARE monitor_id1 	VARCHAR(255);
 	DECLARE monitor_adid1 VARCHAR(36);
 	DECLARE monitor_starttime1 DATETIME DEFAULT '0000-00-00 00:00:00';
@@ -310,7 +312,7 @@ BEGIN
 						WHERE UNIX_TIMESTAMP(inspect_start_time) < UNIX_TIMESTAMP(dragon_time) AND UNIX_TIMESTAMP(inspect_start_time)>(UNIX_TIMESTAMP(dragon_time)-duration) 
 						AND hall_no=dragon_hallno AND monitor_status = 2;
 	WHILE monitor_count != 0 DO
-	FETCH cur1 INTO monitor_order,monitor_starttime,monitor_endtime,monitor_hallno,monitor_statu,monitor_city,monitor_cinemaname,monitor_id;
+	FETCH cur1 INTO monitor_order,monitor_starttime,monitor_endtime,monitor_hallno,monitor_statu,monitor_city,monitor_cinemaname,monitor_ids;
 	
 	IF monitor_statu = 2
 	THEN
@@ -318,19 +320,30 @@ BEGIN
 	#OPEN cur2;
 	#在存储过程中使用触发器调用的话，绝对不能有返回结果的语句。否则再出发起调用存储过程时，存储过程无法执行
 	#SELECT * FROM correct_show WHERE advert_id = monitor_id AND DATE_FORMAT(inspect_start_time,'%y-%m-%d') = DATE_FORMAT(monitor_starttime,'%y-%m-%d');
-	SELECT hallno,advert_id,inspect_order,advert_back,advert_previous INTO correct_hallno,correct_uuid,correct_order,correct_adback,correct_adprevious FROM correct_show
-		WHERE DATE_FORMAT(inspect_start_time,	'%y-%m-%d') = DATE_FORMAT(dragon_time,'%y-%m-%d') AND hallno = dragon_hallno AND advert_id = monitor_id;
+	SELECT hallno,advert_id,inspect_order,advert_back,advert_previous ,imglen INTO correct_hallno,correct_uuid,correct_order,correct_adback,correct_adprevious,correct_imglen FROM correct_show
+		WHERE DATE_FORMAT(inspect_start_time,	'%y-%m-%d') = DATE_FORMAT(dragon_time,'%y-%m-%d') AND hallno = dragon_hallno AND advert_id = monitor_ids;
 	
 	IF correct_order != 0
 	THEN
 	SELECT id,advert_id,inspect_start_time, inspect_end_time INTO monitor_id1,monitor_adid1,monitor_starttime1,monitor_endtime1 FROM oristarmr.app_monitor 
 	WHERE UNIX_TIMESTAMP(inspect_start_time) < UNIX_TIMESTAMP(dragon_time) AND UNIX_TIMESTAMP(inspect_start_time)>(UNIX_TIMESTAMP(dragon_time)-duration) 
-	AND hall_no=dragon_hallno  AND inspect_order = correct_order;
+	AND hall_no=dragon_hallno  AND advert_id = correct_uuid;#AND inspect_order = correct_order;
+	#SELECT inspect_start_time INTO monitor_starttime1 FROM oristarmr.app_monitor WHERE UNIX_TIMESTAMP(inspect_start_time) < UNIX_TIMESTAMP(dragon_time) 
+	#AND UNIX_TIMESTAMP(inspect_start_time)>(UNIX_TIMESTAMP(dragon_time)-duration) AND hall_no=dragon_hallno AND inspect_order = correct_order;
+	#SELECT * FROM oristarmr.app_monitor WHERE UNIX_TIMESTAMP(inspect_start_time) < UNIX_TIMESTAMP(dragon_time) 
+	#AND UNIX_TIMESTAMP(inspect_start_time)>(UNIX_TIMESTAMP(dragon_time)-duration) AND hall_no=dragon_hallno AND inspect_order = correct_order;
 	#INSERT INTO oristarmr.app_monitor (id,advert_id,create_time,advert_back,advert_previous,cinema_city,cinema_name,hall_no,inspect_end_time,inspect_order,inspect_start_time,monitor_status,data_division)
 	#VALUES (REPLACE(UUID(),'-',''),correct_uuid,NOW(),correct_adback,correct_adprevious,monitor_city,monitor_cinemaname,correct_hallno,monitor_endtime1,correct_order,monitor_starttime1,1,1);
-	INSERT INTO oristarmr.app_monitor_rank(id,monitor_id,advert_id,create_time,advert_back,advert_previous,cinema_city,cinema_name,hall_no,inspect_end_time,
-		inspect_order,inspect_start_time,monitor_status,data_division) VALUES(REPLACE(UUID(),'-',''),monitor_id1,monitor_adid1,NOW(),correct_adback,
-		correct_adprevious,monitor_city,monitor_cinemaname,correct_hallno,monitor_endtime1,correct_order,monitor_starttime1,1,1);
+	#INSERT INTO oristarmr.app_monitor_rank(id,monitor_id,advert_id,create_time,advert_back,advert_previous,cinema_city,cinema_name,hall_no,inspect_end_time,
+		#inspect_order,inspect_start_time,monitor_status,data_division) VALUES(REPLACE(UUID(),'-',''),monitor_id1,monitor_adid1,NOW(),correct_adback,
+		#correct_adprevious,monitor_city,monitor_cinemaname,correct_hallno,monitor_endtime1,correct_order,monitor_starttime1,1,1);
+
+	SELECT SUM(imglen) INTO correct_imgcount FROM correct_show WHERE hallno = correct_hallno AND 	inspect_order<= correct_order
+		AND DATE_FORMAT(inspect_start_time,'%y-%m-%d') = DATE_FORMAT(dragon_time,'%y-%m-%d');
+
+	INSERT INTO oristarmr.app_monitor_rank(id,monitor_id,advert_id,create_time,advert_back,advert_previous,cinema_city,cinema_name,hall_no,inspect_end_time,inspect_start_time,
+		inspect_order,monitor_status,data_division)VALUES(REPLACE(UUID(),'-',''),monitor_id1,correct_uuid,NOW(),correct_adback,correct_adprevious,monitor_city,monitor_cinemaname,
+		correct_hallno,FROM_UNIXTIME((UNIX_TIMESTAMP(dragon_time)-correct_imgcount+correct_imglen),'%Y-%m-%d %H:%i:%s'),FROM_UNIXTIME((UNIX_TIMESTAMP(dragon_time)-correct_imgcount),'%Y-%m-%d %H:%i:%s'),correct_order,1,1);
 	END IF;
 	#CLOSE cur2 ;
 
